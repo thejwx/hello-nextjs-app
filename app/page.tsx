04312f4e44db95ca3d68292ad197042a87d3b85c
Jeff Wilson <whackscript@gmail.com>
2025-06-17 15:35:43 -0500

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,22 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
         <h1 className="text-4xl font-bold text-center">
           Hello
-          <form name="new-contact" data-netlify="true">
-            <input type="hidden" name="form-name" value="new-contact" />
-            <p>
-                <label>Name <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Email <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-            </form>
         </h1>
+        <form name="new-contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+          <input type="hidden" name="form-name" value="new-contact" />
+          <p style={{ display: 'none' }}>
+            <label>Don't fill this out if you're human: <input name="bot-field" /></label>
+          </p>
+          <p>
+              <label>Name <input type="text" name="name" required /></label>
+          </p>
+          <p>
+              <label>Email <input type="email" name="email" required /></label>
+          </p>
+          <p>
+              <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     </main>
   )
